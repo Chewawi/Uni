e4a70b09ef8835997ab590bc38a06e31e5abf629
Chewawi <69669283+Chewawi@users.noreply.github.com>
2023-06-07 18:01:15 +0000

    First Commit

--- exts/information.py
+++ exts/information.py
@@ -1,14 +1,20 @@
 from discord.ext import commands 
 
-from core import Uni
-    
+from core import Uni, Embed
+import discord 
+
 class Information(Uni.Cog):
     def __init__(self, bot: 'Uni.Bot') -> None:
         self.bot = bot
 
     @commands.command()
-    async def user(self, ctx: 'Uni.Context')  -> None:
-        await ctx.send("Pong!")
+    async def user(self, ctx: 'Uni.Context', user: 'discord.User')  -> None:
+        embed = Embed(ctx)
+        embed.title = "User Info"
+        embed.add_field(
+            name="_ _ Username", value=f"`{user.name}`"
+        )
+        await ctx.send(embed=embed)
 
 async def setup(bot: 'Uni.Bot') -> None:
     await bot.add_cog(
